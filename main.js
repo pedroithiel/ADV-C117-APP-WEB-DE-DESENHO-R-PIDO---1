2e4359a0f79e1b3dff91fbc0cf44e497f96f4d58
pedroithiel <101153331+pedroithiel@users.noreply.github.com>
2022-12-12 21:38:23 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
-var listaEsboco=["barco", "ambulância", "relógio", "anjo", "avião"];
-var randomNumber  =  Math.floor((Math.random() *listaEsboco.length));
+var listaEsboco = ["barco", "ambulância", "relógio", "anjo", "avião"];
+var randomNumber = Math.floor((Math.random() * listaEsboco.length));
 
 var sketch = listaEsboco[randomNumber];
 
@@ -15,4 +15,35 @@ var drawSketch = "";
 
 var answerHolder = "";
 
- var score = 0;
+var score = 0;
+
+function setup() {
+    var canvas = createCanvas(280, 280);
+    canvas.center();
+
+}
+function draw() {
+    checkSketch();
+    if (drawSketch == sketch) {
+        answerHolder = "set";
+        score++;
+        document.getElementById("pont").innerHTML = score
+    }
+}
+function checkSketch() {
+    timerCounter++;
+    document.getElementById("time");
+    console.log(timerCounter);
+    if(timerCounter == 400) {
+        timerCounter = 0
+        timerCheck = "completed"
+    }
+    if(timerCheck == "completed" || answerHolder  == "set") {
+        timerCheck = ""
+        answerHolder = ""
+        updateCanvas()
+    }
+}
+function updateCanvas() {
+
+}
